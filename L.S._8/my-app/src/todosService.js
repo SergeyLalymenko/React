7975c54fde6692eb767885533baa9b356fd17856
SergeyLalymenko <66252265+SergeyLalymenko@users.noreply.github.com>
2020-11-04 01:39:38 +0300

    L.S._8

--- L.S._8/my-app/src/todosService.js
+++ L.S._8/my-app/src/todosService.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/todos'
+  // baseURL: 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/todos'  основной сервер,может не работать потому что в данных объект сохранился
+  baseURL: 'https://5f4cdc1ceeec51001608e4c8.mockapi.io/todos'  // мой сервер,идентичный основному
 })
